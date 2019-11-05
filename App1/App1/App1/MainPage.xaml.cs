4c8281df44e78437efbb378e3e67da8b96be0f0e
André <andrer@ppedv.de>
2019-11-05 16:01:01 +0100

    Hallo Xamarin

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Class1 cc = new Class1();
+
+            DisplayAlert("Hallo", $"{Battery.ChargeLevel}", "bb");
+        }
+    }
+}
