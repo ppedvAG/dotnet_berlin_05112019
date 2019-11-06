dceb7cd4f15bdc5a45a17fa46058c9a9dec847fd
André <andrer@ppedv.de>
2019-11-06 09:15:28 +0100

    EF Poco

---
+++ RESTClient/EfPocoGen/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EfPocoGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var con = new MyDbContext("Data Source=.;Initial Catalog=Northwind;Integrated Security=True;");
+            var query = con.Employees.OrderBy(x => x.FirstName).ThenByDescending(x => x.HireDate);
+            dataGridView1.DataSource = query.ToList();
+
+            MessageBox.Show(query.ToString());
+        }
+    }
+
+    public partial class MyDbContext
+    {
+        static partial void OnCreateModelPartial(DbModelBuilder modelBuilder, string schema)
+        {
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+            modelBuilder.Entity<Employee>().HasKey(x => x.EmployeeId);
+        }
+    }
+}
